7f4b5c32f4373348850234efec3db38976b3f046
Hyn2 <justin010129@gmail.com>
2023-06-13 01:10:23 +0900

    ✨ feat : Implement and update CRUD functionality for teammate introduction

--- teammate/teammate.js
+++ teammate/teammate.js
@@ -12,7 +12,47 @@ input.forEach(function(element) {
     });
 });
 
+// 값을 받아옴
+document.addEventListener('DOM.teammateRowLoaded', get);
+async function get() {
+    const response = await fetch("http://localhost:8080/api/teammates", {
+        method: 'GET',
+    });
+    const data = await response.json();
+    console.log(data);
+    return data;
+}
+
 // 조원 생성하는 클래스
+const setEditPlaceholder = (arr) => {
+    console.log(arr);
+    const editModal = document.querySelector('.editModal');
+    const inputs = editModal.querySelectorAll('input[type="text"]');
+    const addPreview = editModal.querySelector('img');
+    console.log(inputs);
+    for (let i = 0; i <= inputs.length; i++) {
+        if(i == 0) {
+            addPreview.src = arr[i];
+        } else {
+            inputs[i-1].setAttribute('value' , arr[i]);
+        }
+    }
+};
+
+// 조원 추가 모달 열고 닫기
+const modal = document.querySelector('.modal');
+const openModal= (target, className) => {
+    target.style.display = 'block';
+    setTimeout(()=>{
+        target.classList.add(className);
+    },100);
+};
+const closeModal = (target, className) => {
+    target.classList.remove(className);
+    setTimeout(()=>{
+        target.style.display = 'none';
+    },800);
+};
 class Mate {
     constructor(id,stdId,name,image,part,des,git) {
         this.id = id;
@@ -86,7 +126,6 @@ class Mate {
         githubLink.appendChild(githubA);
         teammate.appendChild(githubLink);
         teammateRow.appendChild(teammate);
-
     } 
 
     setEventListener(index) {
@@ -95,8 +134,15 @@ class Mate {
         deleteButton[index].addEventListener('click', (event)=> {
             if(confirm('없애고 싶은 조원이 있나요?')) {
                 const targetId = event.target.id;
-                deleteTeammate(targetId);
-                alert('삭제되었습니다!');
+                deleteTeammate(targetId)
+                .then(result => {
+                    console.log(result);
+                    alert('삭제되었습니다!');
+                    location.reload();
+                })
+                .catch(error => {
+                    console.error(error);
+                });
             }
         });
         // eventLister for editBtn
@@ -105,8 +151,11 @@ class Mate {
             const targetId = event.target.id;
             getMateById(targetId)
             .then(result => {
-                let arr = Object.values(result);
-                setEditPlaceholder(arr, targetId);
+                const data = Object.values(result);
+                data.shift();
+                setEditPlaceholder(data);
+                const editSubmit = document.querySelector('#editSubmit');
+                editSubmit.value = targetId;
             })
             .catch(error => {
                 console.error(error);
@@ -116,18 +165,6 @@ class Mate {
         });
     }
 
-}
-
-// 값을 받아옴
-const click = document.querySelector('#click');
-document.addEventListener('DOM.teammateRowLoaded', get);
-async function get() {
-    const response = await fetch("http://localhost:8080/api/teammates", {
-        method: 'GET',
-    });
-    const data = await response.json();
-    console.log(data);
-    return data;
 }
 
 // 받아온 값을 처리
@@ -157,7 +194,7 @@ const save = (mateList) => {
     }
 }
 
-// request
+// 특정 조원 삭제
 async function deleteTeammate(id) {
  const response = await fetch(`http://localhost:8080/api/teammates/${id}`, {
     method: 'DELETE',
@@ -173,14 +210,6 @@ async function deleteTeammate(id) {
  return data;
 }
 
-deleteTeammate()
-.then(result => {
-    console.log(result);
-})
-.catch(error => {
-    console.error(error);
-})
-
 // 조원 추가 버튼(Create)
 const addTeammateButton = (text) => {
     // 유효성 검사 구현해야 함
@@ -193,23 +222,64 @@ const addTeammateButton = (text) => {
 }
 addTeammateButton('새로운 팀원이 생겼나요?');
 
-// 조원 추가 모달 열고 닫기
-const modal = document.querySelector('.modal');
-const openModal= (target, className) => {
-    target.style.display = 'block';
-    setTimeout(()=>{
-        target.classList.add(className);
-    },100);
+async function addTeammate() {
+    const addModal = document.querySelector('.modal');
+    const input = addModal.querySelectorAll('input');
+    const requestBody = {
+        student_id: input[0].value,
+        name: input[1].value,
+        profile_image: input[5].value,
+        part: input[2].value,
+        description: input[3].value,
+        github_link: input[4].value,
+    }
+    console.log(requestBody);
+    const response = await fetch('http://localhost:8080/api/teammates', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(requestBody)
+    });
 }
-const closeModal = (target, className) => {
-    target.classList.remove(className);
-    setTimeout(()=>{
-        target.style.display = 'none';
-    },800);
-}
+// 추가 모달 버튼
+const addSubmit = document.querySelector('#addSubmit');
+addSubmit.addEventListener('click',()=> {
+    if(confirm('추가하시겠습니까?')) {
+        addTeammate()
+        .then(result => {
+            console.log(result);
+            alert('추가되었습니다!');
+            location.reload();
+        })
+        .catch(error => {
+            console.error(error);
+        })
+    }
 
-const modalclosebutton = document.querySelector('#closeModal');
+});
+//
+const modalclosebutton = document.querySelector('#closeAddModal');
 modalclosebutton.addEventListener('click',()=>{closeModal(modal,'opacityQue')});
+
+// 수정 모달 버튼
+const editModalSubmitButton = document.querySelector('#editSubmit');
+editModalSubmitButton.addEventListener('click',()=> {
+    if(confirm('정말 수정하시겠습니까?')) {
+        editPut(editModalSubmitButton.value).then(result => {
+            console.log(result);    
+        }).catch(error => {
+            console.error(error);
+        })
+    }
+});
+
+// 수정 모달 닫기
+const editModalCloseButton = document.querySelector('#closeEditModal');
+editModalCloseButton.addEventListener('click',()=> {
+    const editModal = document.querySelector('.editModal');
+    closeModal(editModal, 'editOpacityQue');
+});
 
 // 아이디로 조원 정보 검색 GET
 async function getMateById(id) {
@@ -219,28 +289,20 @@ async function getMateById(id) {
     const data = await response.json();
     return data;
 }
-const editModal = document.querySelector('.editModal');
-const setEditPlaceholder = (arr) => {
-    const editModalInput = editModal.querySelectorAll('input');
-    console.log(arr);
-    for (let i = 0; i < editModalInput.length; i++) {
-        editModalInput[i].setAttribute('value' , arr[i+1]);
-    }
-};
 
-// 수정 정보 입력
-async function editPut() {
+// 수정 요청 보내기
+async function editPut(id) {
     const editModal = document.querySelector('.editModal');
     const input = editModal.querySelectorAll('input');
     const requestBody = {
         student_id: input[0],
         name: input[1],
-        profile_image: input[2],
-        part: input[3],
-        description: input[4],
-        github_link: input[5],
+        profile_image: input[5],
+        part: input[2],
+        description: input[3],
+        github_link: input[4],
     }
-    const response = await fetch(`http://localhost:8080/api/teammates/5`, {
+    const response = await fetch(`http://localhost:8080/api/teammates/${id}`, {
         method: 'PUT',
         headers: {
             'Content-Type': 'application/json'
@@ -251,37 +313,22 @@ async function editPut() {
     return data;
 }
 
-// 수정 모달 추가 버튼
-const editModalSubmitButton = document.querySelector('#editSubmit');
-editModalSubmitButton.addEventListener('click',()=> {
-    editPut().then(result => {
-        console.log(result);    
-    }).catch(error => {
-        console.error(error);
-    })
-});
-// 수정 모달 닫기
-const editModalCloseButton = document.querySelector('#closeEditModal');
-editModalCloseButton.addEventListener('click',()=> {
-    closeModal(editModal, 'editOpacityQue');
-});
-
 const showImage = (event) => {
     let selectedFile = event.target.files[0];
     console.log(selectedFile);
     if(selectedFile) {
         let reader = new FileReader();
-        reader.onload = (uploadEvent) => {
-            let fileURL = uploadEvent.target.result;
-            const imgEle = document.createElement('img');
-            imgEle.src = fileURL;
-            const editModalForm = document.querySelector('#editModalForm');
-            editModalForm.appendChild(imgEle);
-        };
-    
+
+        reader.onload = function(event) {
+            var fileDataUrl = event.target.result;
+
+            var previewImage = document.getElementById("addPreview");
+            previewImage.src = fileDataUrl;
+          };
+
+          reader.readAsDataURL(selectedFile);
     }
 };
-
 const imageUploader = document.querySelector('#addProfileImage');
 imageUploader.addEventListener('change', showImage);
 
