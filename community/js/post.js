f2f24da3ffcb761032422b6f06f4f8eccd0bc66b
gardenKim <oyasumiee2@gmail.com>
2023-06-15 16:26:18 +0900

    ✨feat: add community post writer

--- community/js/post.js
+++ community/js/post.js
@@ -1,141 +1,171 @@
 (() => {
-  // input 요소 클릭 이벤트 
-  let input = document.querySelector('.form-control');
+   // input 요소 클릭 이벤트 
+   let input = document.querySelector('.form-control');
   
-  // 클릭 시 주황색 실선 표시
-  input.addEventListener('click', function() {
-    this.style.borderBottom = '3px solid orange';
-  });
-  // 클릭해제 시 실선 제거
-  input.addEventListener('blur', function() {
-    this.style.borderBottom = 'none';
-  }); 
-  
-  
-  const postForm = document.querySelector("#communityForm");
-  const listRedirect = document.querySelector(".list-btn");
-  
-  postForm.addEventListener("submit", (event) => {
-    event.preventDefault(); // 기본 제출 동작 방지
-  
-    const formData = new FormData(postForm);
-  
-    // 이미지 파일을 formData에 추가 (사진을 선택하지 않았을 경우 빈 배열 추가)
-    const imageInput = document.getElementById("image");
-    const imageFiles = imageInput.files;
-    if (imageFiles.length > 0) {
-      for (let i = 0; i < imageFiles.length; i++) {
-        formData.append("image", imageFiles[i]);
-      }
-    } else {
-      formData.append("image", null); // 사진을 선택하지 않은 경우 빈값
-    }
-  
-  
-    // 서버로 데이터 전송
-    fetch("http://127.0.0.1:8080/api/community", {
-      method: "POST",
-      body: formData,
-    })
-      .then((response) => response.json())
-      .then((data) => {
-        console.log("게시글 작성 성공:", data);
-        
-        // 글 작성이 완료되면 자기가 작성한 글 보는 페이지로 넘어감
-        fetch(`http://127.0.0.1:8080/api/community/${data.post_id}`, {
-          headers: {
-            "Content-Type": "application/json",
-          },
-          method: "GET",
-        })
-          .then((response) => response.json())
-          .then((post) => {
-            // 게시물 디테일 페이지를 가져오고 화면을 변경
-            fetch("../community/detail.html", { credentials: "include" }) // 메인 페이지 요청에도 쿠키를 포함
-              .then((response) => response.text())
-              .then((html) => {
-                // 로그인.html의 내용을 제거하고 메인.html의 내용 추가
-                while (document.documentElement.firstChild) {
-                  document.documentElement.removeChild(
-                    document.documentElement.firstChild
-                  );
-                }
-
-                const search_html = document.querySelector("html");
-                const head = document.createElement("head");
-                const body = document.createElement("body");
-                search_html.appendChild(head);
-                search_html.appendChild(body);
-
-                const range = document.createRange();
-                const parsedHTML = range.createContextualFragment(html);
-                document.body.appendChild(parsedHTML);
-
-                // 메인.html과 관련된 CSS 파일 추가
-                const mainStyle = document.createElement("link");
-                mainStyle.rel = "stylesheet";
-                mainStyle.type = "text/css";
-                mainStyle.href = "../community/css/detail.css";
-                document.head.appendChild(mainStyle);
-
-                // 메인.html과 관련된 JavaScript 파일 추가
-                const mainScript = document.createElement("script");
-                mainScript.src = "../community/js/detail.js";
-                document.body.appendChild(mainScript);
-              })
-              .catch((error) => {
-                console.error("에러:", error);
-              });
-          })
-
-      })
-      .catch((error) => {
-        console.error("게시글 작성 실패:", error);
-      });
-  });
-  
-  // 목록 버튼 누르면 게시글 리스트 페이지로 이동
-  
-  listRedirect.addEventListener("click", () => {
-    fetch("../community/list.html", { credentials: "include" })
-      .then((response) => response.text())
-      .then((html) => {
-        // 해당 html 내용 제거 후 변경할 html로 내용 변경
-        document.documentElement.innerHTML = "";
-  
-        const range = document.createRange();
-        const parsedHTML = range.createContextualFragment(html);
-        document.body.appendChild(parsedHTML);
-  
-        const appContainer = document.querySelector(".app-container");
-  
-        // CSS 파일 추가
-        const mainStyle = document.createElement("link");
-        mainStyle.rel = "stylesheet";
-        mainStyle.href = "../community/css/list.css";
-        document.head.appendChild(mainStyle);
-  
-        // JavaScript 파일 추가
-        const mainScript = document.createElement("script");
-        mainScript.src = "../community/js/list.js";
-        mainScript.type = "text/javascript"; // MIME 유형 설정
-        document.body.appendChild(mainScript);
-
-        let login_state = localStorage.getItem('loginState');
-        // login_state가 "login"일때 로그아웃 생성
-        if (login_state === "login") {
-          // 로그인택스트 변경 
-          const loginText = document.querySelector(".loginText");
-          loginText.innerHTML = "로그아웃";
-          // 로그아웃 컨테이너 클래스명 변경
-          const loginContainer = document.querySelector(".loginContainer");
-          loginContainer.classList = "logoutContainer";
-        }
-      })
-      .catch((error) => {
-        console.error("에러:", error);
-      });
-  });
+   // 클릭 시 주황색 실선 표시
+   input.addEventListener('click', function() {
+     this.style.borderBottom = '3px solid orange';
+   });
+   // 클릭해제 시 실선 제거
+   input.addEventListener('blur', function() {
+     this.style.borderBottom = 'none';
+   }); 
+   
+   
+   const postForm = document.querySelector("#communityForm");
+   const listRedirect = document.querySelector(".list-btn");
+   
+   
+   postForm.addEventListener("submit", (event) => {
+     event.preventDefault(); // 기본 제출 동작 방지
+   
+     fetch("http://127.0.0.1:8080/api/status", {
+       method: "GET",
+       headers: {
+         "Content-Type": "application/json",
+       },
+       credentials: "include",
+     })
+       .then((statusResponse) => {
+         if (!statusResponse.ok) {
+           throw new Error("서버 응답이 실패했습니다.");
+         }
+   
+         return statusResponse.json();
+       })
+       .then((statusResult) => {
+         
+         const nickname = statusResult.user.nickname;
+         const author_id = statusResult.user.id;
+ 
+         // /api/status의 응답을 확인
+         if (statusResult.message === "User is logged in") {
+           // 사용자가 로그인되어 있는 상태
+           console.log("사용자가 로그인되어 있습니다.");
+   
+           // FormData 객체 생성
+           const formData = new FormData(postForm);
+ 
+ 
+           // 이미지 빈값처리(null로 하니까 오류 발생)
+           const imageInput = document.getElementById("image");
+           const imageFiles = imageInput.files;
+ 
+           if (imageFiles.length === 1) {
+             imageFiles[1];
+           } else {
+             formData.append("image", ""); // 사진을 선택하지 않은 경우 빈값
+           }
+ 
+           formData.append("nickname", nickname);
+           formData.append("author_id", author_id);
+ 
+           
+           // 서버로 데이터 전송
+           fetch("http://127.0.0.1:8080/api/community", {
+             method: "POST",
+             body: formData,
+             credentials: "include",
+           })
+             .then((response) => response.json())
+             .then((data) => {
+               console.log("게시글 작성 성공:", data);
+   
+               // 바로 삭제 가능하도록 postId 값을 로컬 스토리지에 저장
+               const post_id = data.post_id;
+               localStorage.setItem("postId", post_id);
+               // 글 작성이 완료되면 자기가 작성한 글 보는 페이지로 넘어감
+               fetch(`http://127.0.0.1:8080/api/community/${post_id}`, {
+                 headers: {
+                   "Content-Type": "application/json",
+                 },
+                 method: "GET",
+                 credentials: "include",
+               })
+                 .then((response) => response.json())
+                 .then((post) => {
+                   // 게시물 디테일 페이지를 가져오고 화면을 변경
+                   fetch("../community/detail.html", {
+                     credentials: "include",
+                   }) // 메인 페이지 요청에도 쿠키를 포함
+                     .then((response) => response.text())
+                     .then((html) => {
+                       // 로그인.html의 내용을 제거하고 메인.html의 내용 추가
+                       while (document.documentElement.firstChild) {
+                         document.documentElement.removeChild(
+                           document.documentElement.firstChild
+                         );
+                       } 
+   
+                       const search_html = document.querySelector("html");
+                       const head = document.createElement("head");
+                       const body = document.createElement("body");
+                       search_html.appendChild(head);
+                       search_html.appendChild(body);
+   
+                       const range = document.createRange();
+                       const parsedHTML = range.createContextualFragment(html);
+                       document.body.appendChild(parsedHTML);
+   
+                       // 메인.html과 관련된 CSS 파일 추가
+                       const mainStyle = document.createElement("link");
+                       mainStyle.rel = "stylesheet";
+                       mainStyle.type = "text/css";
+                       mainStyle.href = "../community/css/detail.css";
+                       document.head.appendChild(mainStyle);
+   
+                       // 메인.html과 관련된 JavaScript 파일 추가
+                       const mainScript = document.createElement("script");
+                       mainScript.src = "../community/js/detail.js";
+                       document.body.appendChild(mainScript);
+                     })
+                     .catch((error) => {
+                       console.error("에러:", error);
+                     });
+                 });
+             })
+             .catch((error) => {
+               console.error("게시글 작성 실패:", error);
+             });
+         } else if (statusResult.message === "User is logged out ") {
+           // 사용자가 로그인되어 있지 않은 상태
+           console.log("사용자가 로그인되어 있지 않습니다.");
+         }
+       })
+       .catch((error) => {
+         console.log("api/status 통신 실패", error);
+       });
+   });
+   
+   // 목록 버튼 누르면 게시글 리스트 페이지로 이동
+   
+   listRedirect.addEventListener("click", () => {
+     fetch("../community/list.html", { credentials: "include" })
+       .then((response) => response.text())
+       .then((html) => {
+         // 해당 html 내용 제거 후 변경할 html로 내용 변경
+         document.documentElement.innerHTML = "";
+   
+         const range = document.createRange();
+         const parsedHTML = range.createContextualFragment(html);
+         document.body.appendChild(parsedHTML);
+     
+         // CSS 파일 추가
+         const mainStyle = document.createElement("link");
+         mainStyle.rel = "stylesheet";
+         mainStyle.href = "../community/css/list.css";
+         document.head.appendChild(mainStyle);
+   
+         // JavaScript 파일 추가
+         const mainScript = document.createElement("script");
+         mainScript.src = "../community/js/list.js";
+         mainScript.type = "text/javascript"; // MIME 유형 설정
+         document.body.appendChild(mainScript);
+       })
+       .catch((error) => {
+         console.error("에러:", error);
+       });
+   }); 
   
   // 자유게시판 연결
   const board = document.querySelector('.board');
